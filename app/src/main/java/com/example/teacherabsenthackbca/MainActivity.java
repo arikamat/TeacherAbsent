0df5d35a09afd9098cb7358f40ee6afa2e5c025d
Ari Kamat <arijkamat@gmail.com>
2020-02-13 22:27:32 -0500

    Revert Java Code

--- app/src/main/java/com/example/teacherabsenthackbca/MainActivity.java
+++ app/src/main/java/com/example/teacherabsenthackbca/MainActivity.java
@@ -2,10 +2,8 @@ package com.example.teacherabsenthackbca;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.*;
 
 public class MainActivity extends AppCompatActivity {
-    //hello world
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
