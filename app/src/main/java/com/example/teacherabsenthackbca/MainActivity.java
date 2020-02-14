292fffa77e4d4bf4a1a95b8f53d630829cd43ab0
Ari Kamat <arijkamat@gmail.com>
2020-02-14 00:04:41 -0500

    Add code in MainActivity.java

--- app/src/main/java/com/example/teacherabsenthackbca/MainActivity.java
+++ app/src/main/java/com/example/teacherabsenthackbca/MainActivity.java
@@ -2,11 +2,83 @@ package com.example.teacherabsenthackbca;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+public class MainActivity extends AppCompatActivity{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
+        TextView trialText = (TextView)findViewById(R.id.trial);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+        String valueRenderOption ="";
+        String dateTimeRenderOption="";
+        String spreadsheetId = "1lbQX5qVgicH8ayeXhmvIJn2puluiH7efxWXJJKtWgZM";
+        String spreadsheetRange="A:B";
+        Sheets sheetsService = createSheetsService();
+        Sheets.Spreadsheets.Values.Get request =
+                sheetsService.spreadsheets().values().get(spreadsheetId, spreadsheetRange);
+        request.setValueRenderOption(valueRenderOption);
+        request.setDateTimeRenderOption(dateTimeRenderOption);
+
+        ValueRange response = request.execute();
+        trialText.setText(response.toString());
+        }
+        catch (IOException e){
+            throw new RuntimeException(e);
+        }
+        catch(GeneralSecurityException f){
+            throw new RuntimeException(f);
+        }
+    }
+    public static Sheets createSheetsService() throws IOException, GeneralSecurityException {
+        HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+
+        // TODO: Change placeholder below to generate authentication credentials. See
+        // https://developers.google.com/sheets/quickstart/java#step_3_set_up_the_sample
+        //
+        // Authorize using one of the following scopes:
+        //   "https://www.googleapis.com/auth/drive"
+        //   "https://www.googleapis.com/auth/drive.file"
+        //   "https://www.googleapis.com/auth/drive.readonly"
+        //   "https://www.googleapis.com/auth/spreadsheets"
+        //   "https://www.googleapis.com/auth/spreadsheets.readonly"
+        GoogleCredential credential = null;
+
+        return new Sheets.Builder(httpTransport, jsonFactory, credential)
+                .setApplicationName("Google-SheetsSample/0.1")
+                .build();
     }
 }
